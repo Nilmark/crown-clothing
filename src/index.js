0b54d69dcdf9b29fb1f6afa5e603e13564041ad0
Nilmark Acain <kramlins19@gmail.com>
2022-11-10 19:25:55 +0800

    Added Cart Icon and implement toggle to open

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./contexts/user.context";
+
 import App from "./App";
+import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
+import { CartProvider } from "./contexts/cart.context";
+
 import reportWebVitals from "./reportWebVitals";
+
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,7 +16,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
